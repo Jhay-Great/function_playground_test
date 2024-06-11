0b9003b5bdb1cedafb6f342a829336b2284f98fa
Jhay-Great <alemonai.jenef@gmail.com>
2024-06-11 15:38:13 +0000

    added pseudo code implementation

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // String transformation
 // capitalize
+/**
+ * input => string
+ * go through the string and take the 1st letter
+ * alt
+ * find the first letter, that is the index of the first letter
+ */
 const test = 'this';
 const capitalize = function(string) {
-    
 }
 
 capitalize(test);
