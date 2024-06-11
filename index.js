c8f67716bdce6a1e4e60b09dcba6b89ca3199341
Jhay-Great <alemonai.jenef@gmail.com>
2024-06-11 15:43:16 +0000

    update algorithm to return the entire input string

--- index.js
+++ index.js
@@ -5,10 +5,11 @@
  * go through the string and take the 1st letter
  * alt
  * find the first letter, that is the index of the first letter
+ * use the replace method to replace the first letter with upperCased letter
  */
 const test = 'this';
 const capitalize = function(string) {
-    return string[0].toUpperCase();
+    return string.replace(string[0], string[0].toUpperCase());
 }
 
 console.log(capitalize(test));
